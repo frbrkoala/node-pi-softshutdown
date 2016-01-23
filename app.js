78ff83828b0a42b3acf264af553358405b0a9f51
nvlasov@sg.ibm.com <nvlasov@sg.ibm.com>
2016-01-23 14:35:37 +0800

    Minor fixes

--- app.js
+++ app.js
@@ -16,25 +16,13 @@ try {
 try {
     board.on('ready', function() {
 
-        // Create a new `button` hardware instance.
-        // This example allows the button module to
-        // create a completely default instance
+        // Create a new "button" hardware instance with onlut on pin 7.
         button = new five.Button(7);
-
-        // Inject the `button` hardware into
-        // the Repl instance's context;
-        // allows direct command line access
-        //board.repl.inject({
-        //  button: button
-        //});
-
-        // Button Event API
 
         // "down" the button is pressed
         button.on("down", function() {
             //  console.log('Button pressed');
             exec(cmd, function(error, stdout, stderr) {
-
                 // command output is in stdout
             });
         });
